396508010d49766b286dd4ce0844a454349965b5
Gaia Lunardi <gaia.lunardi@gmail.com>
2023-02-22 11:42:50 +0100

    translating sinusoid

--- src/charts/Sinusoid.jsx
+++ src/charts/Sinusoid.jsx
@@ -1,7 +1,8 @@
 import { scaleLinear } from 'd3'
 export function Sinusoid({ value, width, height, selected, onTrack }) {
   const bPercentage = 0.3
-  const k = width / 4
+  const NUM_PEAKS = 6
+  const k = width / (NUM_PEAKS - 1)
   const b = k * bPercentage
   const yStartingPoint = height / 2
   const amplitude = value * 10
@@ -10,8 +11,8 @@ export function Sinusoid({ value, width, height, selected, onTrack }) {
     .range([10, height / 2])
   let path = `M 0,${yStartingPoint}`
 
-  ;[...Array(4).keys()].forEach((i) => {
-    const f = i % 2 ? 1 : -1
+  ;[...Array(NUM_PEAKS).keys()].forEach((i) => {
+    const f = i % 2 ? -1 : 1
     path = path.concat(
       ` C ${k * i + b},${yStartingPoint + computeAmplitude(value) * f} ${k * (i + 1) - b},${
         yStartingPoint + computeAmplitude(value) * f
@@ -21,6 +22,7 @@ export function Sinusoid({ value, width, height, selected, onTrack }) {
 
   return (
     <path
+      transform={`translate(${-width / 10})`}
       d={path}
       opacity={selected ? 1 : 0.3}
       stroke="white"

938d6965b50eeb75367cd2769866b1b806ea6d4d
Gaia Lunardi <gaia.lunardi@gmail.com>
2023-02-24 15:09:19 +0100

    used sheet content in card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,13 @@
-export function Card({ id, children, title, subtitle, unit, number }) {
+import content from '../data/content.json'
+export function Card({ id, children, number }) {
   return (
     <div className={`flex flex-col justify-start flex-start bg-${id} p-2 text-white`}>
-      <h1>{title}</h1>
-      <div>{subtitle}</div>
+      <h1>{content[`card_${id}_inf_name`]}</h1>
+      <div>{content[`card_${id}_sci_name`]}</div>
       {children}
       <div className="w-100 flex items-end flex-col">
         <h1>{number}</h1>
-        <div>{unit}</div>
+        <div>{content[`card_${id}_unit`]}</div>
       </div>
     </div>
   )

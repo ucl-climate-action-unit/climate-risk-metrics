84d5303b0e865b54a1cd2d6b111ec176089f290c
Gaia Lunardi <gaia.lunardi@gmail.com>
2023-03-01 16:47:42 +0100

    removed text color from components to make it depend on the section background

--- src/components/Subtitle.jsx
+++ src/components/Subtitle.jsx
@@ -1,7 +1,7 @@
 export function Subtitle({ value, metric }) {
   return (
     <h2
-      className={`font-bold text-black mb-4 ${
+      className={`font-bold mb-4 ${
         metric === 'EEI' ? 'text-EEI' : metric === 'STC' ? 'text-STC' : 'text-UWI'
       }`}
     >
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,7 +1,7 @@
 export function Title({ value, metric }) {
   return (
     <h1
-      className={`font-bold text-5xl text-black mb-4 ${
+      className={`font-bold text-5xl mb-4 ${
         metric === 'EEI' ? 'text-EEI' : metric === 'STC' ? 'text-STC' : 'text-UWI'
       }`}
     >

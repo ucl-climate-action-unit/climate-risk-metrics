3de2a7b7e3a386623d10d0e18afd13d75f192456
Gaia Lunardi <gaia.lunardi@gmail.com>
2023-03-06 11:00:51 +0100

    improved code in summary

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,7 +1,7 @@
 import { IconEEI, IconSTC, IconUWI } from '../icons'
 import { METRIC_IDS } from '../utils/ids'
 
-export function Summary({ content, children }) {
+export function Summary({ content, EEIValue, STCValue, UWIValue }) {
   return (
     <p className="mt-9 mb-16 w-2/3 text-xl block">
       {METRIC_IDS.map((id) => {
@@ -9,9 +9,13 @@ export function Summary({ content, children }) {
           <span key={id}>
             {content[`summary_${id}`]}
             <span className="inline-block ml-2 align-middle">
-              {id === 'EEI' ? <IconEEI /> : id === 'STC' ? <IconSTC /> : <IconUWI />}
+              {id === 'EEI' && <IconEEI />}
+              {id === 'STC' && <IconSTC />}
+              {id === 'UWI' && <IconUWI />}
             </span>
-            <span className="font-bold">{` ${this} ${content[`summary_${id}_unit`]} `}</span>
+            <span className="font-bold">{` ${
+              (id === 'UWI' && UWIValue) || (id === 'STC' && STCValue) || (id === 'EEI' && EEIValue)
+            } ${content[`summary_${id}_unit`]} `}</span>
           </span>
         )
       })}

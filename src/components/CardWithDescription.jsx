dbe6bb7d33a0ba8a33714be96cd9f10e64c1b3d7
Gaia Lunardi <gaia.lunardi@gmail.com>
2023-03-08 15:45:12 +0100

    added anchor link to metric section

--- src/components/CardWithDescription.jsx
+++ src/components/CardWithDescription.jsx
@@ -2,10 +2,12 @@ import React, { useState } from 'react'
 import { BarChart, CurveAmplitudeChart, RotationChart } from '../charts'
 import { Card } from './Card'
 import { useBreakpoints } from '../hooks/useBreakPoints'
+import { Link } from 'react-scroll'
 
 export function CardWithDescription({ id, value, shortDescription, metric }) {
   const { isMd } = useBreakpoints()
   const [isOpen, setIsOpen] = useState(false)
+  console.log(isMd)
   return (
     <div className="flex-1">
       <Card id={id} number={value} isOpen={isOpen} setIsOpen={setIsOpen}>
@@ -16,14 +18,16 @@ export function CardWithDescription({ id, value, shortDescription, metric }) {
       <div className={`${isMd ? 'mb-4' : `duration-700 ${isOpen ? 'mb-4' : 'hidden'}`}`}>
         {shortDescription}
       </div>
-      <a
-        href="/"
-        className={` ${
+      <Link
+        smooth={true}
+        to={id}
+        offset={-80}
+        className={`cursor-pointer ${
           isMd ? 'font-bold underline' : `duration-700 ${isOpen ? 'font-bold underline' : 'hidden'}`
         }`}
       >
         {metric}
-      </a>
+      </Link>
     </div>
   )
 }

da49eb1cf3cfc883a4b59871433468148271d6f6
Gaia Lunardi <gaia.lunardi@gmail.com>
2023-03-10 18:38:50 +0100

    added axis prop to curve amplitude

--- src/charts/CurveAmplitudeChart.jsx
+++ src/charts/CurveAmplitudeChart.jsx
@@ -2,7 +2,7 @@ import { useMeasure } from 'react-use'
 import { Sinusoid } from './Sinusoid'
 import { round } from 'lodash'
 
-export function CurveAmplitudeChart({ value }) {
+export function CurveAmplitudeChart({ axis = true, value }) {
   const [wrapperRef, { width, height }] = useMeasure()
   const PADDINGX = 24
   const PADDINGY = 30
@@ -14,30 +14,34 @@ export function CurveAmplitudeChart({ value }) {
       {width > 0 && height > 0 && (
         <svg width={width} height={height} className="absolute ">
           {/* coordinates for scale are computed empirically */}
-          <text x={width - PADDINGX + 4} y={height * 0.125} fill="white">
-            10
-          </text>
-          <line
-            x1={PADDINGX}
-            y1={height * 0.125 - 1}
-            x2={width - PADDINGX}
-            y2={height * 0.125 - 1}
-            stroke="white"
-            strokeWidth={1}
-            opacity={0.3}
-          />
-          <text x={width - PADDINGX + 4} y={height / 2} fill="white">
-            1
-          </text>
-          <line
-            x1={PADDINGX}
-            y1={height - height * 0.125 + 1}
-            x2={width - PADDINGX}
-            y2={height - height * 0.125 + 1}
-            stroke="white"
-            strokeWidth={1}
-            opacity={0.3}
-          />
+          {axis && (
+            <>
+              <text x={width - PADDINGX + 4} y={height * 0.125} fill="white">
+                10
+              </text>
+              <line
+                x1={PADDINGX}
+                y1={height * 0.125 - 1}
+                x2={width - PADDINGX}
+                y2={height * 0.125 - 1}
+                stroke="white"
+                strokeWidth={1}
+                opacity={0.3}
+              />
+              <text x={width - PADDINGX + 4} y={height / 2} fill="white">
+                1
+              </text>
+              <line
+                x1={PADDINGX}
+                y1={height - height * 0.125 + 1}
+                x2={width - PADDINGX}
+                y2={height - height * 0.125 + 1}
+                stroke="white"
+                strokeWidth={1}
+                opacity={0.3}
+              />
+            </>
+          )}
           <mask id="maskForSinusoids">
             <rect x={PADDINGX} y={0} width={width - 2 * PADDINGX} height={height} fill="white" />
           </mask>

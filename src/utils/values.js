05231292a137d77c1e8708fd50540b7d43ae7c18
Gaia Lunardi <gaia.lunardi@gmail.com>
2023-03-10 18:47:14 +0100

    added computational function for getting values

--- src/utils/values.js
+++ src/utils/values.js
@@ -2,28 +2,45 @@ import EEI_data from '../data/EEI_data.json'
 import STC_data from '../data/STC_data.json'
 import UWI_data from '../data/UWI_data.json'
 import { round } from 'lodash'
+const data = { EEI: EEI_data, STC: STC_data, UWI: UWI_data }
 
 const filterGlobal = (data) => {
   return data.filter((d) => d.region === 'global')
 }
 
-const formatValue = (value, decimals = 2) => round(parseFloat(value), decimals)
+const formatValue = (value, metricId) => round(parseFloat(value), metricId === 'UWI' ? 1 : 2)
 
 const computeCurrent = (prev, current) => {
   return +prev.year >= +current.year && +prev.month > +current.month ? prev : current
 }
 
-export const computeEEIValueGlobalByYear = (year) => {
-  const EEICurrentGlobalData = filterGlobal(EEI_data).reduce((prev, current) =>
-    computeCurrent(prev, current)
+export const computeGlobalValueByYear = (year, metricId) => {
+  return formatValue(
+    filterGlobal(data[metricId])
+      .filter((d) => +d.year === +year)
+      .reduce((prev, current) => (+prev.month > +current.month ? prev : current)).value,
+    metricId
   )
-  return formatValue(EEICurrentGlobalData.value)
+}
+export const computeCurrentRegionValues = (metricId) => {}
+export const computeYearValues = (metricId) => {
+  let decemberValues = []
+  data[metricId].forEach((datum) => {
+    const lastValue = data[metricId]
+      .filter((d) => d.year === datum.year)
+      .reduce((prev, current) => {
+        return +prev.month > +current.month ? prev : current
+      })
+    if (!decemberValues.find((e) => e.year === datum.year)) {
+      decemberValues.push(lastValue)
+    }
+  })
+  return decemberValues
 }
 
 export const computeCurrentGlobalValue = (metricId) => {
-  const data = metricId === 'EEI' ? EEI_data : metricId === 'STC' ? STC_data : UWI_data
-  const STCCurrentGlobalData = filterGlobal(data).reduce((prev, current) =>
+  const currentGlobalDatum = filterGlobal(data[metricId]).reduce((prev, current) =>
     computeCurrent(prev, current)
   )
-  return formatValue(STCCurrentGlobalData.value, metricId === 'UWI' ? 1 : undefined)
+  return formatValue(currentGlobalDatum.value, metricId)
 }

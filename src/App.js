9ca5f3ee5e1966d5409e3a76678763c15d277b9c
Francesco Iacono <f.iacono89@gmail.com>
2023-03-10 11:39:01 +0100

    Add title and favicon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import React, { useEffect } from 'react'
 import './App.css'
 import { IntroSection, DeepeningSection, MenuMobile, Footer } from './components/index'
+import intro from './data/fixed_content.json'
 
 function App() {
+  useEffect(() => {
+    document.title = intro.page_title
+  }, [])
   return (
     <div className="App font-satoshiRegular">
       <MenuMobile />

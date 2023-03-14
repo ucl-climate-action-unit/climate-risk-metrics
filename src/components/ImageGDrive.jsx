ded715fdb0382d325fb889e7a4350857c10d1e98
Francesco Iacono <f.iacono89@gmail.com>
2023-03-14 17:39:04 +0100

    Image dimensions setting a limit on max height

--- src/components/ImageGDrive.jsx
+++ src/components/ImageGDrive.jsx
@@ -2,7 +2,11 @@ export function ImageGDrive({ src, alt }) {
   const regexUrl = new RegExp('^https://drive.google.com/file/d/(.*?)/(.*)')
   return (
     src.match(regexUrl) && (
-      <img alt={alt} src={src.replace(regexUrl, 'https://drive.google.com/uc?export=view&id=$1')} />
+      <img
+        className="max-h-[690px]"
+        alt={alt}
+        src={src.replace(regexUrl, 'https://drive.google.com/uc?export=view&id=$1')}
+      />
     )
   )
 }
